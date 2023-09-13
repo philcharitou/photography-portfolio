4cc990ba5b7d24e1a5f4dc5d055a71f178509c5c
Phil Charitou <phil.charitou@gmail.com>
2023-09-13 18:36:10 +0000

    Misc items from server

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+	host: '159.203.26.31',
+    },
     plugins: [
         laravel({
             input: [
