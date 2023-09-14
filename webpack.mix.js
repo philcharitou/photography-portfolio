dab5ac87eb266c099eb87ea552614a9701623a3c
philoaple <phil@oaple.com>
2023-09-14 08:41:02 -0400

    Updates to webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,3 +3,8 @@
 let mix = require('laravel-mix');
 
 mix.css('resources/css/site.css', 'css').js('resources/js/site.js', 'js');
+
+mix.css('resources/css/site.css', 'css');
+mix.css('resources/css/site.css', 'css');
+mix.css('resources/css/site.css', 'css');
+mix.css('resources/css/site.css', 'css');
