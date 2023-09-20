428eaf99a758614bb4f5e52f70ee324cce4f0f39
philoaple <phil@oaple.com>
2023-09-20 09:59:46 -0400

    Updates for JS

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,8 +4,6 @@ let mix = require('laravel-mix');
 
 mix.js('resources/js/site.js', 'js').postCss('resources/css/site.css', 'css');
 
-mix.js('resources/js/custom.js', 'js');
-
 mix.webpackConfig({
 stats: {
     children: true,
