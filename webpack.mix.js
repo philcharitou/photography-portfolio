db4c58b987de5de41da9e39fe214b189f81662ca
philoaple <phil@oaple.com>
2023-09-28 13:42:31 -0400

    Updating some styles and NPM behaviour

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,7 @@
 
 let mix = require('laravel-mix');
 
-mix.js('resources/js/site.js', 'js').postCss('resources/css/site.css', 'css');
+mix.postCss('resources/css/site.css', 'css');
 
 mix.webpackConfig({
 stats: {
