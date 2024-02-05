9499d6b824e83f7c7cefd01d4bd571218f84ae99
philoaple <phil@oaple.com>
2024-02-05 16:39:12 -0500

    Changes to templates & blueprints

--- database/migrations/2024_01_01_800000_create_permission_tables.php
+++ database/migrations/2024_01_01_800000_create_permission_tables.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -11,6 +12,8 @@ return new class extends Migration
      */
     public function up(): void
     {
+        DB::statement('SET SESSION sql_require_primary_key=0');
+
         $teams = config('permission.teams');
         $tableNames = config('permission.table_names');
         $columnNames = config('permission.column_names');
