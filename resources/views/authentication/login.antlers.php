429d73e3b3914e00f640a50dacb5809b70ca6cbf
philoaple <phil@oaple.com>
2024-02-09 16:30:09 -0500

    Updating login page

--- resources/views/authentication/login.antlers.php
+++ resources/views/authentication/login.antlers.php
@@ -71,7 +71,7 @@
                                             <path fill-rule="evenodd" d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0m-.646 5.354a.5.5 0 0 0-.708-.708L7.5 10.793 6.354 9.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0z"/>
                                         </svg>
                                     </div>
-                                    <span>Save your billing and delivery information to order faster</span>
+                                    <span>{{ account_benefits[0] ?? "Save your billing and delivery information to order faster" }}</span>
                                 </li>
                                 <li class="border-b py-3 flex items-center gap-8">
                                     <div>
@@ -79,7 +79,7 @@
                                             <path d="M2 15.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2zM8 4.41c1.387-1.425 4.854 1.07 0 4.277C3.146 5.48 6.613 2.986 8 4.412z"/>
                                         </svg>
                                     </div>
-                                    <span>Access your session & order history</span>
+                                    <span>{{ account_benefits[1] ?? "Access your session & order history" }}</span>
                                 </li>
                                 <li class="border-b py-3 flex items-center gap-8">
                                     <div>
@@ -87,7 +87,7 @@
                                             <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2M8 1.918l-.797.161A4 4 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4 4 0 0 0-3.203-3.92zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5 5 0 0 1 13 6c0 .88.32 4.2 1.22 6"/>
                                         </svg>
                                     </div>
-                                    <span>Receive the latest news and updates on our promotional or educational materials</span>
+                                    <span>{{ account_benefits[2] ?? "Receive the latest news and updates on our promotional or educational materials" }}</span>
                                 </li>
                             </ul>
                         </div>
